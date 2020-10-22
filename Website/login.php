6053fa78fb972b25c0df86e403b47843b320c765
fortress <david.la.gordt.dillie@gmail.com>
2020-10-22 11:31:17 +0200

    added dal

--- Website/login.php
+++ Website/login.php
@@ -3,13 +3,13 @@
 <head>
       <meta charset="UTF-8">
       <meta name="viewport" content="width=device-width, initial-scale=1.0">
-      <link rel="stylesheet" href='/Website/styles.css'>
+      <link rel="stylesheet" href='styles.css'>
 </head>
 
 
 <body class="login-body">
 
-<?php include($_SERVER['DOCUMENT_ROOT'].'/Website/nav.html') ?>
+<?php include "nav.html" ?>
 
 <div class=login-container>
         <h1>Login</h1>

5f82b918017e39ddf3bd3e091dcdd62f94ee92be
Menderes <61561832+mendokral@users.noreply.github.com>
2020-10-23 14:51:45 +0200

    Profile page latest design

--- Website/views/profile.php
+++ Website/views/profile.php
@@ -7,22 +7,37 @@
     <link rel="stylesheet" href='../styles.css'>
 </head>
 
-<body>
+<body class="profile_body">
     <?php include "shared/nav.html" ?>
 
     <div class="profile_tab_content">
-        <div class=profile-details>
-            <p>First Name:</p>
-            <p>Last Name: </p>
-            <p>Email:</p>
-            <p>Date of Birth:</p>    
-            <p>Phone Number:</p>
-            <p>Address:</p>
-            <input type="button" value="Edit Profile">
+        <div class=profile_details>
+            <div><p>First Name:</p><input type="text" value="Lebron"></div>
+            <div><p>Last Name:</p><input type="text" value="James"></div>
+            <div><p>Email:</p><input type="text" value="lebron.james@email.com"></div>
+            <div><p>Date of Birth:</p><input type="text" value=" 27/09/1970"></div>
+            <div><p>Phone Number:</p><input type="text" value=" 612356782"></div>
+            <div><p>BSN:</p><input type="text" value="123456789"></div>  
+            <div><p>City:</p><input type="text" value="Tilburg"></div>  
+            <div><p>Street:</p><input type="text" value="Biggekerkestraat"></div>  
+            <div><p>House Number:</p><input type="text" value="19"></div>  
+            <input type="button" value="Edit Profile" class="profile_edit">
         </div>
-        <div>
-            <?php include "rentHistory.php" ?>
+        <div class="profile_rent_history">
+            <div class= "profile_rent_history_divs">
+                <?php include "rentHistory.php" ?>
+            </div>
+            <div class= "profile_rent_history_divs">
+                <?php include "rentHistory.php" ?>
+            </div>
+            <div class= "profile_rent_history_divs">
+                <?php include "rentHistory.php" ?>
+            </div>
+            <div class= "profile_rent_history_divs">
+                <?php include "rentHistory.php" ?>
+            </div>
         </div>
+
 
     </div>
 
--- Website/views/rentHistory.php
+++ Website/views/rentHistory.php
@@ -1,6 +1,21 @@
-
 <img src="https://via.placeholder.com/150">
-<p>Brand</p>
-<p>Model</p>
-<p>Date Rented</p>
-<p>Date Returned</p>
+<div>
+      <p>Brand:
+
+Volkswagen</p>
+</div>
+<div>
+      <p>Model:
+
+Polo</p>
+</div>
+<div>
+      <p>Date Rented:
+
+12/10/2020</p>
+</div>
+<div>
+      <p>Date Returned:
+
+22/10/2020</p>
+</div>

9c8173108c87825fdbd3026fcf1e60bfa76c4ddd
fortress <david.la.gordt.dillie@gmail.com>
2020-12-09 11:26:56 +0100

    style for add car/ car overview

--- Website/views/addcar.php
+++ Website/views/addcar.php
@@ -6,54 +6,60 @@ if (isset($_SESSION['role']) && $_SESSION['role'] != "admin") {
 }
 
 ?>
-
 <script src="../js/loadImage.js"> </script>
 
 <body>
-
     <?php
     $currentPage = "Add Car";
     include "shared/adminNav.php";
     ?>
 
-    <div class="sell_form">
-        <form action="../Logic/addcar.php" method="POST">
-            <div class="form-group">
 
-                <img class="uploaded_image" id="output" />
-                <input type="file" name="img" accept="image/*" onchange="loadImage(event)">
+    <form action="../Logic/addcar.php" method="POST">
+        <div class="add_car_form">
+            <div class="grid_item_1">
+                <div class="form-group">
+                    <img class="uploaded_image" id="output" />
+                    <input type="file" name="img" accept="image/*" onchange="loadImage(event)">
+                </div>
+            </div>
+            <div class="grid_item_2">
+                <div class="small_items_grid">
+                    <div class="form-group">
+                        <input type="text" name="brand" placeholder="Brand">
+                    </div>
+                    <div class="form-group">
+                        <input type="text" name="model" placeholder="Model">
+                    </div>
+                    <div class="form-group">
+                        <input type="text" name="year" placeholder="Year">
+                    </div>
+                    <div class="form-group">
+                        <input type="text" name="distance" placeholder="Distance Driven">
+                    </div>
+                </div>
+            </div>
+            <div class="grid_item_3">
+                <div class="small_items_grid">
+                    <div class="form-group">
+                        <input type="text" name="transmission" placeholder="Transmission">
+                    </div>
+                    <div class="form-group">
+                        <input type="text" name="fuel" placeholder="Fuel">
+                    </div>
+                    <div class="form-group">
+                        <input type="text" name="city" placeholder="City">
+                    </div>
+                    <div class="form-group">
+                        <input type="text" name="price" placeholder="Price">
+                    </div>
+                </div>
+            </div>
 
-            </div>
-            <div class="form-group">
-                <input type="text" name="brand" placeholder="Brand">
-            </div>
-            <div class="form-group">
-                <input type="text" name="model" placeholder="Model">
-            </div>
-            <div class="form-group">
-                <input type="text" name="year" placeholder="Year">
-            </div>
-            <div class="form-group">
-                <input type="text" name="distance" placeholder="Distance Driven">
-            </div>
-            <div class="form-group">
-                <input type="text" name="transmission" placeholder="Transmission">
-            </div>
-            <div class="form-group">
-                <input type="text" name="fuel" placeholder="Fuel">
-            </div>
-            <div class="form-group">
-                <input type="text" name="city" placeholder="City">
-            </div>
-            <div class="form-group">
-                <input type="text" name="price" placeholder="Price">
-            </div>
-
-            <div>
+            <div class="grid_item_4">
                 <input type="submit" class="add_button" value="Add Car">
             </div>
-        </form>
-    </div>
-
+        </div>
+    </form>
     </div>
 </body>
--- Website/views/carOverview.php
+++ Website/views/carOverview.php
@@ -1,39 +1,49 @@
 <?php
+include "shared/header.php";
 require "../Logic/carOverview.php";
 
 $momentum = new CarOverview();
 $arr = $momentum->GetCars();
 
+if (isset($_SESSION['role']) && $_SESSION['role'] != "admin") {
+    header('Location: ../Logic/logout.php');
+}
 ?>
 
 <body>
-    <table>
-        <tr>
-            <th>Car ID</th>
-            <th>Brand</th>
-            <th>Model</th>
-            <th>Year</th>
-            <th>Distance Driven</th>
-            <th>Transmission</th>
-            <th>Fuel Type</th>
-            <th>City</th>
-            <th>Price</th>
-            <th>Image</th>
+    <?php
+    $currentPage = "Car Overview";
+    include "shared/adminNav.php";
+    ?>
+    <div class="car_overview_table_div">
+        <table class="car_overview_table">
+            <tr class="car_row_main">
+                <th>Car ID</th>
+                <th>Brand</th>
+                <th>Model</th>
+                <th>Year</th>
+                <th>Distance Driven</th>
+                <th>Transmission</th>
+                <th>Fuel Type</th>
+                <th>City</th>
+                <th>Price</th>
+                <th>Image</th>
 
-        </tr>
-        <?php foreach ($arr as $rows) : ?>
-            <tr class="car_row">
-                <td> <?php print_r($rows['car_id']) ?> </td>
-                <td> <?php print_r($rows['brand']) ?> </td>
-                <td> <?php print_r($rows['model']) ?> </td>
-                <td> <?php print_r($rows['year']) ?> </td>
-                <td> <?php print_r($rows['distance_driven']) ?> </td>
-                <td> <?php print_r($rows['transmission']) ?> </td>
-                <td> <?php print_r($rows['fuel_type']) ?> </td>
-                <td> <?php print_r($rows['city']) ?> </td>
-                <td> <?php print_r($rows['price']) ?> </td>
-                <td> <?php echo '<img src="data:image/jpeg;base64,' . base64_encode($rows['img']) . '"/>'; ?> </td>
             </tr>
-        <?php endforeach; ?>
-    </table>
+            <?php foreach ($arr as $rows) : ?>
+                <tr class="car_row">
+                    <td> <?php print_r($rows['car_id']) ?> </td>
+                    <td> <?php print_r($rows['brand']) ?> </td>
+                    <td> <?php print_r($rows['model']) ?> </td>
+                    <td> <?php print_r($rows['year']) ?> </td>
+                    <td> <?php print_r($rows['distance_driven']) ?> </td>
+                    <td> <?php print_r($rows['transmission']) ?> </td>
+                    <td> <?php print_r($rows['fuel_type']) ?> </td>
+                    <td> <?php print_r($rows['city']) ?> </td>
+                    <td> <?php print_r($rows['price']) ?> </td>
+                    <td> <?php echo '<img src="data:image/jpeg;base64,' . base64_encode($rows['img']) . '"/>'; ?> </td>
+                </tr>
+            <?php endforeach; ?>
+        </table>
+    </div>
 </body>
